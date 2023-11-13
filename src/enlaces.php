e36713f97a4bc46cc096a268efe6d67b186e0293
JorgeReveron <jorgealexanderclases@gmail.com>
2023-11-13 18:47:40 +0000

    list finish, add links

---
+++ src/enlaces.php
@@ -0,0 +1,13 @@
+<h1>Enlaces</h1>
+<p>
+  <a href="list.php?type=products">Productos</a>
+</p>
+<p>
+  <a href="list.php?type=services">Servicios</a>
+</p>
+<p>
+  <a href="list.php?type=date-of-expiration">Elementos con fecha de expiracion</a>
+</p>
+<p>
+  <a href="list.php?type=not-expired">Elementos vendibles</a>
+</p>
--- src/list.php
+++ src/list.php
@@ -1,11 +1,49 @@
 <?php 
 require_once "tienda.php";
+?>
+  <p>
+    <a href="enlaces.php">Listado de enlaces</a>
+  </p>
+<?php
 
-$action = isset($_GET["action"]) ? $_GET["action"] : "null";
+$action = isset($_GET["type"]) ? $_GET["type"] : "null";
 
+switch($action) {
+  case "products" :
+    foreach ($tienda as $item) {
+      if ($item instanceof Productos) { ?>
+        <p><?= $item->getName() ?>: <?= $item->getCharacteristics() ?> : <?= $item->getTotalPrice() ?>€</p>
+      <?php }
+    }
+    break;
+  case "services" :
+    foreach ($tienda as $item) {
+      if ($item instanceof Eventos || $item instanceof Sesiones || $item instanceof Mixtos || $item instanceof Normales) { ?>
+        <p><?= $item->getName() ?>: <?= $item->getCharacteristics() ?> : <?= $item->getTotalPrice() ?>€</p>
+      <?php }
+    }
+    break;
+  case "date-of-expiration" :
+    foreach ($tienda as $item) {
+      if ($item instanceof Perecederos || $item instanceof Eventos || $item instanceof Mixtos) { ?>
+        <p><?= $item->getName() ?>: <?= $item->getCharacteristics() ?> : <?= $item->getTotalPrice() ?>€</p>
+      <?php }
+    }
+    break;
+  case "not-expired" :
+    foreach ($tienda as $item) {
+      if ($item instanceof Perecederos) { 
+        if ($item->expired() == "false"){
+          ?>
+          <p><?= $item->getName() ?>: <?= $item->getCharacteristics() ?> : <?= $item->getTotalPrice() ?>€</p>
+          <?php }
+        }
+    }
+    break;
 
-
-foreach ($tienda as $item) { ?>
-<p><?= $item->getName() ?>: <?= $item->getCharacteristics() ?> : <?= $item->getTotalPrice() ?>€</p>
-
-<?php } ?>
+  default :
+    foreach ($tienda as $item) { ?>
+      <p><?= $item->getName() ?>: <?= $item->getCharacteristics() ?> : <?= $item->getTotalPrice() ?>€</p>
+    <?php }
+    break;
+}
